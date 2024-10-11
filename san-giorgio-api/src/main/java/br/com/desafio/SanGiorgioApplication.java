ec85d8d12956191931e50bf49fd5888ec6d177d2
Benigno <benigno.sales@netbiis.com>
2024-10-11 11:21:57 -0300

    config: configurações

--- san-giorgio-api/src/main/java/br/com/desafio/SanGiorgioApplication.java
+++ san-giorgio-api/src/main/java/br/com/desafio/SanGiorgioApplication.java
@@ -1,9 +1,11 @@
 package br.com.desafio;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@EnableRabbit
 @SpringBootApplication
 public class SanGiorgioApplication {
     public static void main(String[] args) {
